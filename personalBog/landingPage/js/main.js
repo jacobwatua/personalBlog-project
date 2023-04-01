5fd9dc89dbc2f432e47a07e9744b250acaf8fe59
jacobwatua <jacobwatua254@gmai.com>
2023-04-01 14:15:51 +0300

    Here are the corrections for the project

--- personalBog/landingPage/js/main.js
+++ personalBog/landingPage/js/main.js
@@ -1,18 +1,54 @@
 /**
- * displaySlide - this function displays picture slides in the slideshow-container 
- * @param n: The current slide to be displayed
- * @returns: no return value 
- */
-function displaySlide (n) {
-    var slides  = document.getElementsByClassName("mySlides");
-    function resetZindex () {
-        for (var i = 0; i < slides.length; i++) {
-            slides[i].style.zIndex = 0;
+  * showOverflowingContent - function that enables a slideshow to ...
+  * show overflowing content by scrolling horizontally.
+  * @function
+  * @name showOverflowingContent
+  * @example
+  * showOverflowingContent();
+  * @returns {void}
+*/
+
+
+function showOverflowingContent() {
+    const container = document.querySelector('.slideshow-container');
+    const nextBtn = document.querySelector('.next-slide');
+    const slides = document.querySelectorAll('.slide');
+    let currentSlide = 0;
+    nextBtn.addEventListener('click', () => {
+      if (currentSlide < slides.length - 1) {
+        currentSlide++;
+        container.scrollLeft += container.offsetWidth;
+      } else {
+        currentSlide = 0;
+        container.scrollLeft = 0;
+      }
+    });
+    slides.forEach((slide, index) => {
+      slide.addEventListener('click', () => {
+        if (index === slides.length - 1 && currentSlide === index) {
+          currentSlide = 0;
+          container.scrollLeft = 0;
         }
-    }
-    resetZindex();
-    slides[n].style.zIndex = 1;
+      });
+    });
+  }
+
+
+
+/**
+ * showPreviousOverflow - function that enables a slideshow to show previous ...
+ * overflowing content by scrolling horizontally.
+ * @function
+ * @name showPreviousOverflow
+ * @example
+ * showPreviousOverflow();
+ * @returns {void}
+*/
+
+function showPreviousOverflow() {
+  const container = document.querySelector('.slideshow-container');
+  const previousBtn = document.querySelector('.previous');
+  previousBtn.addEventListener('click', () => {
+    container.scrollLeft -= container.offsetWidth;
+  });
 }
-
-
-
